b78ad265f6c235a71e587a3afdb81c051ff84605
etn-muguet <nmnm000@kakao.com>
2025-07-11 11:31:50 +0900

    movieTableViewCell 파일 생성

---
+++ SeSACWeek3/movieTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  movieTableViewCell.swift
+//  SeSACWeek3
+//
+//  Created by 김현지 on 7/11/25.
+//
+
+import UIKit
+
+class movieTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
